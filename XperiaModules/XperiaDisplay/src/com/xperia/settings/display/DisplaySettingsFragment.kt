950c8357df186e79a26a9f5eaf90d2554e07697e
Alcatraz323 <alcatraz32323@gmail.com>
2025-04-06 21:37:09 +0500

    XperiaDisplay: Implement Motion Blur Reduction, White Balance adjustment
    [ahnet-69]: THIS HAS BEEN REVERSED FROM THE WORK OF @Alcatraz323 , I TAKE NO CREDIT FOR THE CODE CHANGES IN THIS COMMIT.
    (i have given authorship too pls dont bomb me)
    
    * Video Enhancement has not been implemented because the hal is cooked, I'll probably revisit it later.
    * Move XperiaDisplay's sepolicy rules into its own folder (so its only compiled when someone uses XperiaDisplay)
    * Continue using Kcal to tune rgb values

--- XperiaModules/XperiaDisplay/src/com/xperia/settings/display/DisplaySettingsFragment.kt
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/DisplaySettingsFragment.kt
@@ -27,18 +27,23 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 import com.xperia.settings.display.R
-import com.xperia.settings.display.CreatorModeUtils.Companion.CREATOR_MODE_ENABLE
-
-const val CREATOR_MODE_KEY = "switchCreatorMode"
+import com.xperia.settings.display.SemcDisplayUtils.Companion.CREATOR_MODE_ENABLE
+import com.xperia.settings.display.SemcDisplayUtils.Companion.MOTION_BLUR_REDUCTION_ENABLE
+import com.xperia.settings.display.SemcDisplayUtils.Companion.WHITE_BALANCE_PROF
 
 class DisplaySettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
-    private lateinit var creatorModeUtils: CreatorModeUtils
 
-companion object {
-    private const val DOT_INDICATOR_SIZE = 12
-    private const val DOT_INDICATOR_LEFT_PADDING = 6
-    private const val DOT_INDICATOR_RIGHT_PADDING = 6
-}
+    companion object {
+        private const val DOT_INDICATOR_SIZE = 12
+        private const val DOT_INDICATOR_LEFT_PADDING = 6
+        private const val DOT_INDICATOR_RIGHT_PADDING = 6
+
+        const val CREATOR_MODE_KEY = "switchCreatorMode"
+        const val WHITE_BALANCE_KEY = "white_balance_switch"
+        const val MOTION_BLUR_KEY = "motion_blur_reduction"
+    }
+
+    private lateinit var semcDisplayUtils: SemcDisplayUtils
 
     private val KEY_CREATOR_MODE_PREVIEW = "creator_mode_preview"
     private val PAGE_VIEWER_SELECTION_INDEX = "page_viewer_selection_index"
@@ -53,27 +58,51 @@ companion object {
     private var mViewPagerImages: Array<View?>? = null
 
     private lateinit var creatorModePreference: SwitchPreferenceCompat
+    private lateinit var motionBlurPreference: SwitchPreferenceCompat
+    private lateinit var whiteBalancePreference: ListPreference
 
     private val settingsObserver = object : ContentObserver(Handler()) {
         override fun onChange(selfChange: Boolean) {
-            creatorModePreference.isChecked = Settings.Global.getInt(requireContext().contentResolver, CREATOR_MODE_ENABLE, 0) != 0
+            creatorModePreference.isChecked = semcDisplayUtils.isCMEnabled()
+
+            motionBlurPreference.isChecked = semcDisplayUtils.isMotionBlurReductionEnabled()
+
+            whiteBalancePreference.isEnabled = !semcDisplayUtils.isCMEnabled()
+            whiteBalancePreference.setValueIndex(semcDisplayUtils.getWbProfile())
         }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.display_settings)
-        creatorModeUtils = CreatorModeUtils(requireContext())
+        semcDisplayUtils = SemcDisplayUtils(requireContext())
 
         addViewPager()
 
         creatorModePreference = findPreference<SwitchPreferenceCompat>(CREATOR_MODE_KEY)!!
-        creatorModePreference.isChecked = creatorModeUtils.isEnabled
+        creatorModePreference.isChecked = semcDisplayUtils.isCMEnabled()
         creatorModePreference.onPreferenceChangeListener = this
+
+        whiteBalancePreference = findPreference<ListPreference>(WHITE_BALANCE_KEY)!!
+        whiteBalancePreference.isEnabled = !semcDisplayUtils.isCMEnabled()
+        whiteBalancePreference.setValueIndex(semcDisplayUtils.getWbProfile())
+        whiteBalancePreference.onPreferenceChangeListener = this
+
+        motionBlurPreference = findPreference<SwitchPreferenceCompat>(MOTION_BLUR_KEY)!!
+        motionBlurPreference.isChecked = semcDisplayUtils.isMotionBlurReductionEnabled()
+        motionBlurPreference.onPreferenceChangeListener = this
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-        when(preference.key) {
-            CREATOR_MODE_KEY -> creatorModeUtils.setMode(newValue as Boolean)
+        when (preference.key) {
+            "motion_blur_reduction" -> {
+                semcDisplayUtils.setMotionBlurReductionEnabled(newValue as Boolean)
+            }
+            "white_balance_switch" -> {
+                semcDisplayUtils.setWhiteBalance((newValue as String).toInt())
+            }
+            "switchCreatorMode" -> {
+                semcDisplayUtils.setCMMode(newValue as Boolean)
+            }
         }
         return true
     }
@@ -186,6 +215,17 @@ companion object {
             true,
             settingsObserver
         )
+        requireContext().contentResolver?.registerContentObserver(
+            Settings.Global.getUriFor(MOTION_BLUR_REDUCTION_ENABLE),
+            true,
+            settingsObserver
+        )
+        requireContext().contentResolver?.registerContentObserver(
+            Settings.Global.getUriFor(WHITE_BALANCE_PROF),
+            true,
+            settingsObserver
+        )
+
         settingsObserver.onChange(false, null)
     }
 
