442bd52b62136b3b1dee41d651dd1d1f710d9a34
ahnet-69 <syedmusicrealise@gmail.com>
2025-04-29 04:15:22 +0500

    XperiaDisplay: Implement Video Enhancement

--- XperiaModules/XperiaDisplay/src/com/xperia/settings/display/SemcDisplayUtils.kt
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/SemcDisplayUtils.kt
@@ -16,6 +16,7 @@ import vendor.semc.hardware.display.V2_0.IDisplay
 import vendor.semc.hardware.display.V2_0.IDisplayCallback
 import vendor.semc.hardware.display.V2_0.PccMatrix
 import vendor.semc.hardware.display.V2_2.IFramerateController
+import vendor.semc.hardware.extlight.IExtLight
 
 class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
 
@@ -36,6 +37,7 @@ class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
 
         const val CREATOR_MODE_ENABLE = "cm_enable"
         const val MOTION_BLUR_REDUCTION_ENABLE = "motion_blur_reduction_enable"
+        const val VIDEO_ENHANCE_ENABLE = "video_image_enhance_enable"
         const val WHITE_BALANCE_PROF = "white_balance_profile"
     }
 
@@ -55,6 +57,12 @@ class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
             ?: throw Exception("SEMC FramerateController HIDL not found")
     }
 
+    private val extLightService by lazy {
+        IExtLight.Stub.asInterface(
+            ServiceManager.waitForDeclaredService("${IExtLight.DESCRIPTOR}/default")
+        ) ?: throw IllegalStateException("SEMC ExtLight AIDL not found")
+    }
+
     private val kcalUtils = KcalUtils()
 
     fun isCMEnabled(): Boolean =
@@ -62,6 +70,9 @@ class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
 
     fun getWbProfile(): Int =
         Settings.Global.getInt(context.contentResolver, WHITE_BALANCE_PROF, 4)
+
+    fun isVideoImageEnhanceEnabled(): Boolean =
+        Settings.Secure.getInt(context.contentResolver, VIDEO_ENHANCE_ENABLE, 0) != 0
 
     fun isMotionBlurReductionEnabled(): Boolean =
         Settings.Global.getInt(context.contentResolver, MOTION_BLUR_REDUCTION_ENABLE, 0) != 0
@@ -75,6 +86,11 @@ class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
         if(!enable) setWhiteBalance(getWbProfile())
 
         Settings.Global.putInt(context.contentResolver, CREATOR_MODE_ENABLE, if (enable) 1 else 0)
+    }
+
+    fun setVideoEnhanceEnabled(enabled: Boolean) {
+        extLightService.setExtHdr(if (enabled) 1 else 0)
+        Settings.Secure.putInt(context.contentResolver, VIDEO_ENHANCE_ENABLE, if (enabled) 1 else 0)
     }
 
     fun setMotionBlurReductionEnabled(enable: Boolean) {
@@ -92,6 +108,9 @@ class SemcDisplayUtils(private val context: Context) : IDisplayCallback.Stub() {
             setCMMode(true)
         } else {
             setWhiteBalance(getWbProfile())
+        }
+        if (isVideoImageEnhanceEnabled()) {
+            setVideoEnhanceEnabled(true)
         }
         if (isMotionBlurReductionEnabled()) {
             setMotionBlurReductionEnabled(true)
---
+++ interfaces/semc/hardware/extlight/vendor/semc/hardware/extlight/IExtLight.aidl
@@ -0,0 +1,6 @@
+package vendor.semc.hardware.extlight;
+
+@VintfStability
+interface IExtLight {
+    int setExtHdr(int value);
+}
